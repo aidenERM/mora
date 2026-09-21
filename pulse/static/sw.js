a644f4bf5adaaf3f8d6ee1a5ca1df06793fd5b26
aidenERM <175857237+aidenERM@users.noreply.github.com>
2026-09-21 01:33:57 -0500

    Expose iPhone companion pairing flow

--- pulse/static/sw.js
+++ pulse/static/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "pulse-shell-v13";
-const SHELL = ["/", "/static/styles.css", "/static/app.js?v=pulse-0.2-phone-7", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "pulse-shell-v14";
+const SHELL = ["/", "/static/styles.css", "/static/app.js?v=pulse-0.2-phone-8", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
