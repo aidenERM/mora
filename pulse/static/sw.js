cab66e9c6e4418779ce395df9c3e57e7011926a3
aidenERM <175857237+aidenERM@users.noreply.github.com>
2026-09-21 20:54:25 -0500

    Refresh PWA shell for action bridge

--- pulse/static/sw.js
+++ pulse/static/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "pulse-shell-v20";
-const SHELL = ["/", "/static/styles.css", "/static/app.js?v=pulse-0.2-phone-14", "/manifest.webmanifest", "/icon.svg"];
+const CACHE = "pulse-shell-v21";
+const SHELL = ["/", "/static/styles.css", "/static/app.js?v=pulse-0.2-phone-15", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
